c7ab522e6454e9616d8f45c1b6baf31965524738
Rafael Costa <rafael.scosta@picpay.com>
2023-11-07 19:21:33 -0300

    feat: entity para aggregate

--- app/Domain/Repository/AssignedPokemonRepositoryInterface.php
+++ app/Domain/Repository/AssignedPokemonRepositoryInterface.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Domain\Repository;
 
-use App\Domain\Entity\AssignedPokemon;
+use App\Domain\Aggregate\AssignedPokemon;
 
 interface AssignedPokemonRepositoryInterface
 {
